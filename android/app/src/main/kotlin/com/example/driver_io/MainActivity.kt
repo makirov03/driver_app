e8772e177de431623b36c9d0ac74ac8e8ee50572
Hajymammet <hajymammetmak03@gmail.com>
2023-04-07 01:04:51 +0500

    First commit

---
+++ android/app/src/main/kotlin/com/example/driver_io/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.driver_io
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:driver_io/phone.dart';
+import 'package:driver_io/verify.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: 'phone',
+    debugShowCheckedModeBanner: false,
+    routes: {
+      'phone': (context) => const MyPhone(),
+      'verify': (context) => const MyVerify()
+    },
+  ));
+}
